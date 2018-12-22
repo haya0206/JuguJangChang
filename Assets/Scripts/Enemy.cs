0fe7718e55de51d9ed1c5a6d8ce87c6873e4b4db
haya0206 <haya0206@naver.com>
2018-12-22 19:02:18 +0900

    .
    .

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    Transform target;
+    Vector3 direction;
+    private float velocity;
+    public float speed = 4f; // 구체 속도
+    public float damage = 1f; // 데미지
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        direction = (target.position - transform.position).normalized;
+        velocity = speed * Time.deltaTime;
+        float distance = Vector3.Distance(target.position, transform.position);
+        this.transform.position = new Vector3(transform.position.x + (direction.x * velocity),
+                                                   transform.position.y + (direction.y * velocity),
+                                                     transform.position.z + (direction.z * velocity));
+
+    }
+     void OnTriggerEnter2D(Collider2D other)
+     {      
+         if (other.gameObject.tag == "Player")
+         {
+            Time.timeScale = 0.0f;
+            Debug.Log("사망");
+         }
+ 
+         if (other.gameObject.tag == "Weapon")
+         {
+             Destroy(this.gameObject);
+         }
+     }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject player;
+    public float spawnTime = 0.2f;
+    float preTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > preTime + spawnTime){
+            
+            preTime = Time.time;
+        }
+    }
+}
