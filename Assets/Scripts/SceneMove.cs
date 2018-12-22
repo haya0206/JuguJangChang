4c969b8615b284266a5a90a2b40ddcbab2b7bb15
Plurdis <uutak2000@naver.com>
2018-12-22 16:45:05 +0900

    Create 'FirstPage' / SceneMove, Change Background

---
+++ Assets/Scripts/SceneMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void MoveScene(string SceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
