023f78e60fbe2cfdae350fe903892b1018def615
haya0206 <haya0206@naver.com>
2018-12-22 20:17:12 +0900

    new
    new

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -18,7 +18,18 @@ public class EnemySpawner : MonoBehaviour
     void Update()
     {
         if(Time.time > preTime + spawnTime){
-            
+            float dist = 0;
+            Vector2 random = Random.insideUnitSphere * 10;
+            random.x += player.transform.position.x;
+            random.y += player.transform.position.y;
+            while(true){
+                dist = Vector2.Distance(random, player.transform.position);
+                if(dist > 3) break;
+                random = Random.insideUnitSphere * 10;
+                random.x += player.transform.position.x;
+                random.y += player.transform.position.y;
+            }
+            Instantiate(enemy, random, Quaternion.identity);
             preTime = Time.time;
         }
     }
