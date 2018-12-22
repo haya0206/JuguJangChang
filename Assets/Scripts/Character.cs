900e7a0a26457bb58628ba4014c8a286e274f268
haya0206 <haya0206@naver.com>
2018-12-22 16:06:46 +0900

    character
    .

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float moveSpeed = 8f;
+    public Joystick joystick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.up * joystick.Vertical);
+        if (moveVector != Vector3.zero)
+        {
+            transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
